8d7a4ae2a029e63a2e34b370f95189ec6575fc19
Marcus <m41smith@student.bridgew.edu>
2021-09-22 16:09:00 -0400

    -Added Customer.java

---
+++ src/com/company/Customer.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private int CustomerID;
+    private String name;
+    private ArrayList<Week4_BankAccount> accounts;
+}
