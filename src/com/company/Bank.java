55648c34b5bac4b2ea74ea73c4cce93179ee2eca
Marcus <m41smith@student.bridgew.edu>
2021-09-29 15:03:31 -0400

    -Added closeAccount to Customer.java
    -Added several lines of code to Bank.java

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Bank {
@@ -23,10 +24,81 @@ public class Bank {
                     addCustomer(inputReader);
                     break;
                 case 3:
-                    System.out.println("Not yet implemented");
+                    System.out.print("What is the customer ID of the customer to select:");
+                    var idToFind = inputReader.nextInt();
+                    var customer = getCustomer(idToFind);
+                    if (customer.isPresent()){
+                        doCustomerMenu(inputReader, customer.get());
+                    }
+                    else
+                        System.out.println("No such customer exists at this bank");
                     break;
             }
         }
+    }
+
+    private void doCustomerMenu(Scanner inputReader, Customer customer) {
+        while (true) {
+            printCustomerMenu();
+            //ask the user which choice they want and read that in
+            System.out.print("Enter Selection");
+            var choice = inputReader.nextInt();
+            //create a switch statement to select among the choices
+            switch (choice){
+                case 1:
+                    BankAccount newAccount = addAccountToCustomer(inputReader, customer);
+                    allAccounts.add(newAccount);
+                    break;
+                case 2:
+                    closeAccount(inputReader, customer);
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Please choose one of the choices in the list");
+            }
+        }
+    }
+
+    private void closeAccount(Scanner inputReader, Customer customer) {
+        //ask the user for the account number to close
+        System.out.println("Enter id of account to close:");
+        var closeID = inputReader.nextInt();
+        //call close account on the customer
+        var closedAccount = customer.closeAccount(closeID);
+        //if successful remove the account from all accounts as well.
+        if (closedAccount.isPresent()){
+            System.out.println("Closing account ...");
+            allAccounts.remove(closedAccount.get());
+        }
+    }
+
+    private BankAccount addAccountToCustomer(Scanner inputReader, Customer cust) {
+        //ask the user how much the initial balance should be
+        System.out.println("How much will your initial balance be");
+        var initialDeposit = inputReader.nextDouble();
+        //call open account in the customer
+        var newAccount = cust.openAccount(initialDeposit);
+        System.out.println("Created account with Id"+ newAccount.getAccountID());
+        //return the newly created account
+        return newAccount;
+    }
+
+    private void printCustomerMenu() {
+        System.out.println("###############################################");
+        System.out.println("# Please select what to do with this customer #");
+        System.out.println("#    [1] open account                         #");
+        System.out.println("#    [2] close account                        #");
+        System.out.println("#    [3] return to main menu                  #");
+        System.out.println("###############################################");
+    }
+
+    private Optional<Customer> getCustomer(int CustomerID){
+        for (var currentCustomer : allCustomers){
+            if (currentCustomer.getID()==CustomerID)
+                return Optional.of(currentCustomer);
+        }
+        return Optional.empty();
     }
 
     private void addCustomer(Scanner inputReader) {
